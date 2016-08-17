a8fe9b176c78358a1db2ab53b2ffdf43331444ad
Doug Blank <doug.blank@gmail.com>
2016-08-16 23:34:09 -0400

    Need to update Ubuntu

--- project2100.py
+++ project2100.py
@@ -6,13 +6,18 @@ from __future__ import division, print_function, with_statement
 ## Rhine Singleton & Doug Blank
 ## http://www.ecologyandevolution.org/2100home.html
 
-import gtk
+#import gtk
 import freenect
 import chuck
 import os
 import numpy as np
 from PIL import Image
 import time
+import sys
+
+ctx = freenect.init()
+dev = freenect.open_device(ctx, freenect.num_devices(ctx) - 1)
+freenect.set_depth_format(dev, 
 
 average = 14.0 # add to each, in celsius; not needed
 
@@ -199,23 +204,20 @@ class Snare(chuck.FileRead):
         self.setLooping(0)
 
 def get_scan():
-    try:
-        image = freenect.sync_get_depth()[0]
-    except:
-        image = None
-    return image
+     try:
+        scan = freenect.sync_get_depth(format=freenect.DEPTH_MM)[0]
+     except:
+        scan = None
+     return scan
 
-def convert_depth(depth):
-    """
-    depth: A numpy array with 2 bytes per pixel
-    """
-    depth = np.copy(depth)
-    np.clip(depth, 0, 2**10 - 1, depth)
-    depth >>= 2
-    depth = (depth - 205)/50.0 * 255
-    depth = depth.astype(np.uint8)
-    # Scale it:
-    return depth
+def get_depth(scan):
+     """
+     The depth range is 40cm to 800cm, represented as 0 to 255.
+     """
+     depth = scan/30.0
+     depth = depth.astype(np.uint8)
+     depth[0:479, 630:639] = depth[0:479, 620:629]
+     return depth
 
 def start_server(name="chuck", user="turtle"):
     # Start chuck server:
@@ -229,7 +231,7 @@ initialize_data()
 
 instr1 = chuck.MoogSynthesizer()
 #instr1.setFilterQ(0)
-#instr1.setFilterSweepRate(0)
+#instr1.setFilterSweepRate(0.2)
 #instr1.setVibrato(0, 0)
 #instr1.setAfterTouch(0)
 instr1.connect()
@@ -250,7 +252,8 @@ def key_to_freq(n):
 def tdata_to_freq(celsius):
     # convert celsius to freq
     # historic data is 50% of 88 key range
-    minc = min(tdata.values())
+    # the -1.2 at the end of the minc value makes higher lowest note.-RS
+    minc = min(tdata.values())-1.2
     maxc = HIGHEST_DEVIATION # max(tdata.values())
     scale = (celsius - minc) / (maxc - minc)
     key = int(scale * 88)
@@ -267,20 +270,48 @@ def play_note(instrument, percussion, frequency, volume, quarter_note,
     instrument.setFrequency(frequency)
     if count == 1:
         v1 = 1.0
-        v2 = 0.5
+        v2 = 0.7
     else:
-        v1 = 0.5
-        v2 = 0.5
+        v1 = 0.7
+        v2 = 0.7
     if percussion:
         play_percussion(percussion, v1)
     instrument.setGain(volume)
-    instrument.noteOn(1.0)
+    #for synth, use noteOn; for mandolin, use pluck
+    #instrument.noteOn(1.0)
+    instrument.pluck(0.3)
     time.sleep(seconds1)
     # present!
     if percussion: # 2050
         play_percussion(percussion, v2)
     time.sleep(seconds2)
-    instrument.noteOff(0.5)
+    #instrument.noteOff(0.5)
+    # mandolin: use pluck, no noteoff
+    time.sleep(quarter_note * (1.0 - note_percent))
+
+def play_note2(instrument, percussion, frequency, volume, quarter_note, 
+              note_percent, count, year):
+    seconds1 = quarter_note / 2.0
+    seconds2 = max((quarter_note * note_percent) - seconds1, 0)
+    instrument.setFrequency(frequency)
+    if count == 1:
+        v1 = 1.0
+        v2 = 0.7
+    else:
+        v1 = 0.7
+        v2 = 0.7
+    if percussion:
+        play_percussion(percussion, v1)
+    instrument.setGain(volume)
+    #for synth, use noteOn; for mandolin, use pluck
+    #instrument.noteOn(1.0)
+    instrument.pluck(0.3)
+    time.sleep(seconds1)
+    # present!
+    #if percussion: # 2050
+        #play_percussion(percussion, v2)
+    #time.sleep(seconds2)
+    #instrument.noteOff(0.5)
     # mandolin: use pluck, no noteoff
     time.sleep(quarter_note * (1.0 - note_percent))
 
@@ -312,9 +343,9 @@ def play_measure(instrument, percussion, year, angle, tempo):
     tfreq3 = tdata_to_freq(tdata3)
 
     quarter_note = 1.0 - tempo/1.0 
-    eighth_note = quarter_note / 2.0
+    eighth_note = quarter_note/2.0
 
-    if year > 2016: # present!
+    if False and year > 2016: # present!
         instr2.setFrequency(tfreq3)
         instr2.pluck(0.7)
 
@@ -324,49 +355,88 @@ def play_measure(instrument, percussion, year, angle, tempo):
     play_note(instrument, percussion, tfreq2, 0.7, 
               quarter_note, note_percent, 2, year)
 
-    play_note(instrument, percussion, tfreq1, 0.5, 
+    play_note(instrument, percussion, tfreq2, 0.6, 
               quarter_note, note_percent, 3, year)
 
-if __name__ == "__main__":
-    window = gtk.Window()
-    window.fullscreen()
-    window.show()
-    #start_server()
-    #chuck.init()
+
+def play_measure2(instrument, percussion, year, angle, tempo):
+    ## higher tempo?
+    # year 1900 to 2100
+    # angle 0 to 1
+    # temp 0 to 1
+    note_percent = 0.75 # percent of beat that note plays
+
+    #tdata1 = get_or_make_estimate(year - 2, angle)
+    #tdata2 = get_or_make_estimate(year - 1, angle)
+    tdata3 = get_or_make_estimate(year, angle)
+
+    #tfreq1 = tdata_to_freq(tdata1)
+    #tfreq2 = tdata_to_freq(tdata2)
+    tfreq3 = tdata_to_freq(tdata3)
+
+    quarter_note = 1.0 - tempo/1.0 
+    #triplet = quarter_note * 1.333333
+    whole_note = quarter_note * 3
+
+    if False and year > 2016: # present!
+        instr2.setFrequency(tfreq3)
+        instr2.pluck(0.7)
+
+    play_note2(instrument, percussion, tfreq3, 1.0, whole_note,
+              note_percent, 1, year)
+
+    play_note2(instrument, percussion, tfreq3, 0, quarter_note,
+              note_percent, 2, year)
+
+    play_note2(instrument, percussion, tfreq3, 0, quarter_note,
+              note_percent, 3, year)
+
+
+def main():
+    #window = gtk.Window()
+    #window.fullscreen()
+    #window.show()
+    chuck.init()
 
     # Start infinite loop:
+    #year = int(sys.argv[1])
+    #angle = float(sys.argv[2])
     while True:
         # get scan:
+        print("scan!")
         scan = get_scan()
-        if scan is None:
-            break
-        data = convert_depth(scan) # 0 is close, 255 is far away
-        rows, cols = data.shape
-        maximum = 0
-        maximum_col = 0
-        minimum = 255
-        minimum_col = 0
-        pic = np.zeros(shape=(int(rows * .33), cols), dtype="uint8")
-        r = 0
-        for row in range(int(rows * .33), int(rows * .66)): # 480 middle third
-            for col in range(cols): # 640
-                pic[r][col] = data[row][col]
-                if data[row][col] > 0:
+        if not scan:
+            continue
+        else:
+            data = get_depth(scan) # 0 is close, 255 is nothing
+            rows, cols = data.shape
+            minimum = 255
+            minimum_col = 0
+            pic = np.zeros(shape=(5, cols), dtype="uint8")
+            r = 0
+            for row in range(240, 240 + 5): # middle 10
+                c = 0
+                for col in range(100, cols - 200, 1): # 640
                     if data[row][col] < minimum:
                         minimum = data[row][col]
-                        minimum_col = col
-                    if data[row][col] > maximum:
-                        maximum = data[row][col]
-                        maximum_col = col
-                pic[r][col] = data[row][col]
-            r += 1
-        angle = minimum_col/640.0
-        print("min angle:", angle, "distance:", minimum)
-        time.sleep(2)
-        tempo = (year - 1900)/230.0
-        year = minimum/255.0 * 200.0 + 1900
-        play_measure(instr1, percussion, year, angle, tempo)
-        #image = Image.fromarray(pic, mode="L")
+                        minimum_col = c
+                    pic[r][col] = data[row][col]
+                    c += 1
+                r += 1
+            angle = minimum_col/c
+            print("min angle:", angle, "distance:", minimum)
+        year = int(minimum/255.0 * 200.0 + 1900)
+        tempo = (year - 1900)/220.0
+        print("year:", year, "tempo:", tempo)
+        play_measure(instr2, percussion, year, angle, tempo)
+        print("temp:", tdata.get(year, None))
+        #image = Image.fromarray(data, mode="L")
         #image.save("test1.png")
         #print(image)
+        #time.sleep(5)
+        #if year >= 2100:
+        #    initialize_data()
+        #year += 1
 
+if __name__ == "__main__":
+    main()
